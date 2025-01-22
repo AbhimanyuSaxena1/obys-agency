4366a1c2e499082281e4b515658a89e91e50198b
Abhimanyu <abhimanyusaxena1>
2025-01-22 09:42:24 +0530

    implement responsive locomotive scroll functionality and refactor LoaderAnimation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+var mm = gsap.matchMedia();
+mm.add("(min-width:601px)",function(){
+//desktop
 function locomotiveScrollTrigger() {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -37,10 +41,6 @@ function locomotiveScrollTrigger() {
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 }
-locomotiveScrollTrigger()
-var mm = gsap.matchMedia();
-mm.add("(min-width:601px)",function(){
-//desktop
 function LoaderAnimation() {
   var tl = gsap.timeline();
   var count = 0;
@@ -328,6 +328,7 @@ function navAnime(){
 }
 CursorAnimation();
 LoaderAnimation();
+locomotiveScrollTrigger();
 navAnime();
 VidCursor();
 goeyEffect();
@@ -336,7 +337,45 @@ footerAnimation();
 });
 mm.add("(max-width:601px)",function(){
   //phone
+  function locomotiveScrollTrigger() {
+    gsap.registerPlugin(ScrollTrigger);
   
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+  
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+  }
   function LoaderAnimation() {
     var tl = gsap.timeline();
     var count = 0;
@@ -348,6 +387,7 @@ mm.add("(max-width:601px)",function(){
       duration: 0.6,
       delay: 0.2,
       stagger: 0.2,
+
     });
     tl.from("#line1-part1", {
       opacity: 0,
@@ -403,7 +443,12 @@ mm.add("(max-width:601px)",function(){
     tl.to("#loader", {
       opacity: 0,
       delay: -0.1,
+
     });
+    tl.to("#main",{
+      overflow:"hidden",
+
+    })
     tl.from("#page1", {
       y: "150%",
       opacity: 0,
@@ -415,15 +460,17 @@ mm.add("(max-width:601px)",function(){
     tl.from("nav", {
       opacity: 0,
     });
-    tl.from("#hero1 h4,#hero2 h4,#hero3 h2,#hero3 h3,#hero3 h4,#hero4 h4", {
+    tl.from("#hero1 h4,#hero2 h4,#hero3 h2,#hero3 h3,#hero3 h4,#hero4 h4,#page2 #vidContain", {
       y: "120%",
       delay: -0.7,
       stagger: 0.1,
-    });
+    });    
     tl.to("#loader", {
       display: "none",
+      // overflow:"visible",
       // backgroundColor:"grey",
     });
+    
   }
   // function CursorAnimation() {
   //   const mainElement = document.querySelector("#main");
@@ -596,6 +643,7 @@ mm.add("(max-width:601px)",function(){
   // }
   // CursorAnimation();
   LoaderAnimation();
+locomotiveScrollTrigger()
   VidCursor();
   // navAnime();
   // goeyEffect();
